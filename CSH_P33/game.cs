8bbbd495af3d65af2cca544bce8c8f1c7f46ac70
svitlyi <svitlyi_a@itstep.academy>
2024-12-17 18:47:17 +0200

    Add project files.

---
+++ CSH_P33/game.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel;
+
+namespace Game
+{
+    enum CharacterRace
+    {
+        Human,
+        Ork,
+        Elf
+    }
+
+    class Character
+    {
+        public string? name;
+        int health;
+        int damage;
+        int defence;
+        CharacterRace race;
+
+        public CharacterRace Race
+        {
+            get { return race; }
+            set { race = value; }
+        }
+
+        public int Health
+        {
+            get { return this.health; }
+            set { this.health = Math.Max(value, 0); }
+        }
+
+        public Character(string name, int health, int damage, int defence, CharacterRace race)
+        {
+            this.name = name;
+            this.health = health;
+            this.damage = damage;
+            this.defence = defence;
+            this.race = race;
+        }
+
+        public Character() : this("Jonny", 100, 5, 0, CharacterRace.Human) { }
+
+        public void Print()
+        {
+            Console.WriteLine($" -< {this.name} >-");
+            Console.WriteLine($" Здоров\'я: {this.health}");
+            Console.WriteLine($" Шкода: {this.damage}");
+            Console.WriteLine($" Захист: {this.defence}");
+            Console.WriteLine($" Раса: {this.race}");
+        }
+
+        public int TakeDamage(int damage)
+        {
+            this.health = Math.Max(this.health - damage, 0);
+            return damage;
+        }
+
+        public int Attack(Character target)
+        {
+            Random rnd = new Random(Convert.ToInt32(DateTimeOffset.Now.ToUnixTimeSeconds()));
+            int final_damage = Math.Max(this.damage + rnd.Next(-3, 3), 0);
+            return target.TakeDamage(final_damage);
+        }
+
+        public bool IsAlive()
+        {
+            return this.health > 0;
+        }
+    }
+
+    class Berserk : Character
+    {
+        public Berserk(string name, int health, int damage, int defence, CharacterRace race)
+            : base(name, health, damage, defence, race) { }
+        public Berserk() : this("Jonny", 100, 5, 0, CharacterRace.Human) { }
+    }
+}
---
+++ CSH_P33/program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using Game;
+
+Console.OutputEncoding = UTF8Encoding.UTF8;
+Console.InputEncoding = UTF8Encoding.UTF8;
+
+Random rnd = new Random();
+
+Berserk player1 = new Berserk();
+player1.Print();
+Console.WriteLine();
+
+
+Character player2 = new Character("James", 90, 8, 0, CharacterRace.Ork);
+player2.Print();
+
+while (player1.IsAlive() && player2.IsAlive())
+{
+    Console.WriteLine();
+    int player1_damage = player1.Attack(player2);
+    Console.WriteLine($"{player1.name} атакував {player2.name} і наніс {player1_damage} шкоди.");
+    Console.WriteLine($"У {player2.name} залишилось {player2.Health} здоров\'я.");
+    int player2_damage = player2.Attack(player1);
+    Console.WriteLine($"{player2.name} атакував {player1.name} і наніс {player2_damage} шкоди.");
+    Console.WriteLine($"У {player1.name} залишилось {player1.Health} здоров\'я.");
+}
+
+
+Console.WriteLine("\n");
+player1.Print();
+player2.Print();
+
+
