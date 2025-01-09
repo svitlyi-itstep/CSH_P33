0b5c214119da2055c4a937f48f9c393a67cdfaeb
svitlyi <svitlyi_a@itstep.academy>
2025-01-09 19:01:21 +0200

    Lesson code

--- CSH_P33/Lesson_6.cs
+++ CSH_P33/Lesson_6.cs
@@ -1,19 +1,19 @@
-﻿using System.Text;
-using System.Drawing;
-using Chess;
+﻿//using System.Text;
+//using System.Drawing;
+//using Chess;
 
 
-class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.OutputEncoding = UTF8Encoding.UTF8;
-        Console.InputEncoding = UTF8Encoding.UTF8;
-        Chessmen knight = new Knight(1, 1);
-        foreach (Point p in knight.PossibleMoves())
-        { Console.WriteLine(p); }
+//class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        Console.OutputEncoding = UTF8Encoding.UTF8;
+//        Console.InputEncoding = UTF8Encoding.UTF8;
+//        Chessmen knight = new Knight(1, 1);
+//        foreach (Point p in knight.PossibleMoves())
+//        { Console.WriteLine(p); }
 
-        Console.WriteLine($"Can go to 1, 2? {knight.IsCanMove(new Point(1, 2))}");
+//        Console.WriteLine($"Can go to 1, 2? {knight.IsCanMove(new Point(1, 2))}");
 
-    }
-}
+//    }
+//}
---
+++ CSH_P33/Lesson_7.cs
@@ -0,0 +1,65 @@
+﻿namespace CSH_P33
+{
+
+    // АГРЕГАЦІЯ ТА КОМПОЗИЦІЯ
+    // Студент: ім'я, список оцінок, середній бал
+    //      Розрахунок середнього балу
+    //      Подача інформації про студента у вигляді рядка
+    class Student
+    {
+        // 1. Перелічення полів та властивостей класу
+        string? name;
+        List<int> grades;
+        double avgGrade;
+
+        public string? Name { get { return this.name; } set { this.name = value; } }
+        public List<int> Grades { get { return this.grades; } }
+        public double AvgGrade { get { return this.avgGrade; } }
+
+        // 2. Створення конструкторів (з параметрами та за замвочанням)
+        public Student(string? name, int[] grades)
+        {
+            this.name = name;
+            this.grades = new List<int>(grades);
+            this.CountAvgGrade();
+        }
+
+        public Student(string? name)
+        {
+            this.name = name;
+            this.grades = new List<int>();
+            this.avgGrade = 0;
+        }
+
+        public Student() : this("Student") { }
+
+        // 3. Реалізація методів класу
+        public double CountAvgGrade()
+        {
+            if (this.grades.Count == 0)
+            {
+                this.avgGrade = 0;
+                return 0;
+            }
+            this.avgGrade = (double)this.grades.Sum() / this.grades.Count;
+            return this.avgGrade;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.name} ({this.avgGrade}): {String.Join(", ", this.grades)}";
+        }
+    }
+
+    internal class Lesson_7
+    {
+        static public void Main(string[] args)
+        {
+            Student student = new Student("John", new int[] { 4, 3, 6, 1 });
+            student.Grades.Add(5);
+            student.CountAvgGrade();
+            Console.WriteLine(student);
+        }
+        
+    }
+}
