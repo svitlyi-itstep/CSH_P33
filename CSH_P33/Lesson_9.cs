e9f976c473d51cdddac236b13ff1d157a3011803
svitlyi <svitlyi@L01-01>
2025-01-21 20:08:58 +0200

    Lesson Code

--- CSH_P33/Lesson_9.cs
+++ CSH_P33/Lesson_9.cs
@@ -1,12 +1,34 @@
 ﻿using System.Text;
+using System.Net.Http;
+using System.Text.Json;
 
 namespace CSH_P33
 {
+    class MeowFactsData
+    {
+        public List<string> data { get; set; }
+    }
+
     class Lesson_9
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            var httpClient = new HttpClient();
 
+            string url = "https://meowfacts.herokuapp.com/";
+
+            var response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var asJson = await response.Content.ReadAsStringAsync();
+                var facts = JsonSerializer.Deserialize<MeowFactsData>(asJson);
+
+                foreach (var fact in facts.data)
+                {
+                    Console.WriteLine(fact);
+                }
+            }
         }
     }
 }
