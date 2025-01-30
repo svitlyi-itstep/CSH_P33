5b39786e7130a81d75c29eca9b8b4e777710acc8
svitlyi <svitlyi_a@itstep.academy>
2025-01-30 19:48:06 +0200

    Lesson Code

--- CSH_P33/DeckOfCardsAPI/main.cs
+++ CSH_P33/DeckOfCardsAPI/main.cs
@@ -4,14 +4,14 @@ namespace CSH_P33.DeckOfCardsAPI
 {
     class Program
     {
-        public static void Main(string[] args)
-        {
-            Deck deck = new Deck("yzmuk6jhwx9f");
-            Console.WriteLine(deck.Id);
-            Console.WriteLine(deck.Remaining);
-            Console.WriteLine(deck.Shuffled);
+        //public static void Main(string[] args)
+        //{
+        //    Deck deck = new Deck("yzmuk6jhwx9f");
+        //    Console.WriteLine(deck.Id);
+        //    Console.WriteLine(deck.Remaining);
+        //    Console.WriteLine(deck.Shuffled);
 
-            List<int> nums = new List<int>();
-        }
+        //    List<int> nums = new List<int>();
+        //}
     }
 }
---
+++ CSH_P33/Lesson10.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CSH_P33
+{
+    class MeowFactsResponse
+    {
+        [JsonPropertyName("data")]
+        public List<string> Data { get; set; }
+    }
+    
+    class Lesson10
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            Console.InputEncoding = UTF8Encoding.UTF8;
+
+            string date = "2025-01-01";
+            DateTime dateTime = DateTime.ParseExact(date, "yyyy-MM-dd", null);
+            if(dateTime < DateTime.Now) { /* Дата вже пройшла */ }
+            else { /* Дата ще не настала */ }
+
+            //HttpClient client = new HttpClient();
+            //string url = "https://meowfacts.herokuapp.com/";
+            //var response = client.GetAsync(url).Result;
+            //if(response.IsSuccessStatusCode)
+            //{
+            //    Console.WriteLine(response);
+            //    string data = response.Content.ReadAsStringAsync().Result;
+            //    Console.WriteLine(data);
+            //    MeowFactsResponse? facts = JsonSerializer.Deserialize<MeowFactsResponse>(data);
+            //    if(facts is null) throw new NullReferenceException();
+
+            //    foreach(var fact in facts.Data)
+            //    {
+            //        Console.WriteLine(fact);
+            //    }
+            //}
+        }
+    }
+}
