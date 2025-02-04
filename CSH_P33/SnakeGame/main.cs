242269055b22de96db54e05e1b5a0f72ed1a5586
svitlyi <svitlyi_a@itstep.academy>
2025-02-04 20:32:36 +0200

    Lesson code

--- CSH_P33/SnakeGame/main.cs
+++ CSH_P33/SnakeGame/main.cs
@@ -7,12 +7,40 @@ namespace CSH_P33.SnakeGame
     {
         public static void Main(string[] args)
         {
-            Point point = new Point();
-            List<Point> segments = new List<Point>();
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            Console.InputEncoding = UTF8Encoding.UTF8;
+            Console.CursorVisible = false;
+            Game game = new Game();
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKey key = Console.ReadKey(true).Key;
+                    if (key == ConsoleKey.Escape) { break; }
+                    else if (key == ConsoleKey.W || key == ConsoleKey.UpArrow)
+                        game.ChangeSnakeDirection(Direction.UP);
+                    else if (key == ConsoleKey.S || key == ConsoleKey.DownArrow)
+                        game.ChangeSnakeDirection(Direction.DOWN);
+                    else if (key == ConsoleKey.A || key == ConsoleKey.LeftArrow)
+                        game.ChangeSnakeDirection(Direction.LEFT);
+                    else if (key == ConsoleKey.D || key == ConsoleKey.RightArrow)
+                        game.ChangeSnakeDirection(Direction.RIGHT);
+                }
 
-            segments.Add(point);
-            segments.Insert(0, point);
-            segments.RemoveAt(segments.Count - 1);
+                game.Update();
+                game.Draw();
+                if (game.IsGameOver) { break; }
+            }
         }
     }
 }
+
+
+/*
+    Point point = new Point();
+    List<Point> segments = new List<Point>();
+
+    segments.Add(point);
+    segments.Insert(0, point);
+    segments.RemoveAt(segments.Count - 1);
+*/
--- CSH_P33/SnakeGame/snake.cs
+++ CSH_P33/SnakeGame/snake.cs
@@ -1,36 +1,51 @@
 ﻿namespace CSH_P33.SnakeGame
 {
+    enum Direction { UP, DOWN, LEFT, RIGHT }
     class Snake
     {
-        /*
-         * Властивості класу:
-         * - Список сегментів змії;
-         * - Напрям руху змії (enum з списком напрямів);
-         * - Символ голови змії;
-         * - Символ тіла змії.
-         * 
-         * Методи:
-         * - Отримати голову змії;
-         * - Отримати хвіст змії;
-         * - Перевірка зіткнення з тілом змії;
-         * - Видалити хвіст;
-         * - Оновлення стану змії (Update).
-         */
+        //* Властивості класу:
+        //* - Список сегментів змії;
+        //* - Напрям руху змії(enum з списком напрямів);
+        public Direction direction = Direction.RIGHT;
+         //* - Символ голови змії;
+         //* - Символ тіла змії.
+
+         //* Методи:
+         //* - Отримати голову змії;
+         //* - Отримати хвіст змії;
+         //* - Перевірка зіткнення з тілом змії;
+         //* - Видалити хвіст;
+         //* - Оновлення стану змії(Update).
     }
 
     class Game
     {
-        /*
-         * Властивості класу:
-         * - Розмір поля;
-         * - Список точок, в яких знаходиться їжа;
-         * - Об'єкт змії;
-         * - Прапорець GameOver.
-         * 
-         * Методи:
-         * - Оновлення стану гри (Update);
-         * — Малювання гри (Draw);
-         * - Зміна напряму руху змії.
-         */
+
+        //* Властивості класу:
+        //* - Розмір поля;
+        //* - Список точок, в яких знаходиться їжа;
+        //* - Об'єкт змії;
+        Snake snake = new Snake();
+        //* - Прапорець GameOver.
+        public bool IsGameOver = false;
+
+        //* Методи:
+        //* - Оновлення стану гри (Update);
+        public void Update() { }
+        //* — Малювання гри(Draw);
+        public void Draw()
+        {
+            Console.SetCursorPosition(0, 0);
+            this.DrawSnakeDirection();
+        }
+        public void DrawSnakeDirection() 
+        {
+            if (snake.direction == Direction.UP) Console.Write("⬆️");
+            else if (snake.direction == Direction.DOWN) Console.Write("⬇️");
+            else if (snake.direction == Direction.LEFT) Console.Write("⬅️");
+            else if (snake.direction == Direction.RIGHT) Console.Write("➡️");
+        }
+        //* - Зміна напряму руху змії.
+        public void ChangeSnakeDirection(Direction direction) { snake.direction = direction; }
     }
 }
